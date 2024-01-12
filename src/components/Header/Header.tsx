3231718e96696657d12d65b0c52806a7e59f03b8
phantomphd1218 <phantomphd1218@gmail.com>
2024-01-11 21:31:30 -0600

    Finished up to authentication page setup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import Link from "next/link";
+import { useContext } from "react";
 import {FaUserCircle} from "react-icons/fa";
-import {MdDarkMode} from "react-icons/md";
+import {MdDarkMode, MdOutlineLightMode} from "react-icons/md";
+import ThemeContext from "@/context/themeContext";
+
 const Header = () => {
+    const {darkTheme, setDarkTheme} = useContext(ThemeContext)
     return (
         <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
             <div className="flex items-center w-full md:2/3">
@@ -13,7 +19,17 @@ const Header = () => {
                         </Link>
                     </li>
                     <li className="ml-2">
-                        <MdDarkMode className="cursor-pointer" />
+                        {darkTheme ? (<MdOutlineLightMode className="cursor-pointer"
+                        onClick={() => {
+                            setDarkTheme(false);
+                            localStorage.removeItem('hotel-theme');
+                        }}
+                         />) : (<MdDarkMode className="cursor-pointer" 
+                         onClick={() => {
+                            setDarkTheme(true);
+                            localStorage.setItem('hotel-theme', "true");
+                        }}
+                         />)}
                     </li>
                 </ul>
             </div>
